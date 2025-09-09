1a469356afe0f6711105ef4a99ffa292c49e77ba
Igor Cardoso <iygorsilva@gmail.com>
2025-09-09 20:00:16 -0300

    feat(client): add weather forecast with zip code input and api integration

--- forecast-client/src/App.jsx
+++ forecast-client/src/App.jsx
@@ -1,33 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [zip, setZip] = useState("")
+  const [temperature, setTemperature] = useState(null)
+  const [error, setError] = useState(null)
+
+  const handleFetch = async () => {
+    if (!zip) return
+    try {
+      setError(null)
+      setTemperature(null)
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/forecast`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ zip: zip })
+      })
+
+      if (!res.ok) {
+        throw new Error("Invalid or not found ZIP Code")
+      }
+      const data = await res.json()
+      setTemperature(data.currentTemperature)
+      setZip("")
+    } catch (err) {
+      setError('An error occurred during the search, please try again!')
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-container">
+        <div className="card">
+          <h1>🌤️ Weather by ZIP Code</h1>
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="ZIP Code"
+              value={zip}
+              onChange={(e) => setZip(e.target.value)}
+            />
+            <button onClick={handleFetch}>Search</button>
+          </div>
+
+          {temperature !== null && (
+            <p className="result">Current temperature: <strong>{temperature} °C</strong></p>
+          )}
+
+          {error && (
+            <p className="error">{error}</p>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
