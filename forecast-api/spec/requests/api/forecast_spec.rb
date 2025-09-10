5770e6c8896ab518b3b046534e1c103ea752788e
Igor Cardoso <iygorsilva@gmail.com>
2025-09-09 22:10:15 -0300

    feat(api): update forecast request rspec test

--- forecast-api/spec/requests/api/forecast_spec.rb
+++ forecast-api/spec/requests/api/forecast_spec.rb
@@ -1,7 +1,56 @@
 require 'rails_helper'
 
-RSpec.describe "Api::Forecasts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Forecast API", type: :request do
+  let(:zip) { '110001' }
+  let(:geo) { { lat: 12.34, lon: 56.78, address: 'Some Address' } }
+  let(:temperature) { 25.678 }
+
+  before do
+    # Limpa o cache antes de cada teste
+    Rails.cache.clear
+
+    # Mocka os serviços externos
+    allow(GeocodingService).to receive(:extract_zip_and_coords).and_return(geo)
+    allow(WeatherService).to receive(:fetch_by_coords)
+      .and_return({ 'data_current' => { 'temperature' => temperature } })
+  end
+
+  context 'when zip is invalid' do
+    before do
+      allow(GeocodingService).to receive(:extract_zip_and_coords)
+        .with('00000').and_return({ lat: nil, lon: nil })
+    end
+
+    it 'returns status 400 and error message' do
+      post "/api/forecast", params: { zip: '00000' }
+
+      expect(response).to have_http_status(:bad_request)
+      json = JSON.parse(response.body)
+      expect(json['error']).to be true
+      expect(json['message']).to eq('')
+    end
+  end
+
+  context 'when zip is valid' do
+    it 'returns temperature, address and cached false on first request, cached true on second request' do
+      allow(Rails.cache).to receive(:exist?).and_return(false)
+      allow(Rails.cache).to receive(:fetch).and_call_original
+
+      post "/api/forecast", params: { zip: zip }
+      first_json = JSON.parse(response.body)
+
+      expect(first_json['currentTemperature']).to eq(temperature.round(1))
+      expect(first_json['address']).to eq(geo[:address])
+      expect(first_json['cached']).to be false
+
+      allow(Rails.cache).to receive(:exist?).and_return(true)
+
+      post "/api/forecast", params: { zip: zip }
+      second_json = JSON.parse(response.body)
+
+      expect(second_json['currentTemperature']).to eq(temperature.round(1))
+      expect(second_json['cached']).to be true
+      expect(second_json['address']).to eq(geo[:address])
+    end
   end
 end
