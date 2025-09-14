08fcf7256d0143a1febbe078b1358a1be8fe59d6
Igor Cardoso <iygorsilva@gmail.com>
2025-09-14 08:35:21 -0300

    feat: add Custom Fibonacci Sequence

---
+++ challenges/custom_fibonacci_sequence.rb
@@ -0,0 +1,23 @@
+# LINK: https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/custom-fibonacci-sequence/problem
+
+
+def getAutoSaveInterval(n)
+  return 1 if n == 0
+  return 2 if n == 1
+
+  # Para n >= 2
+
+  a, b = 1, 2
+  (2..n).each do
+    # temp = a
+    # a = b
+    # b = temp + b
+
+    a, b = b, a + b
+  end
+  
+  b
+end
+
+
+puts getAutoSaveInterval(10)
