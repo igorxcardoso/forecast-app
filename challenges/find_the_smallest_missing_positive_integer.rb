74180ccd370c0722c3508dce5b5af9fc6bdf7b05
Igor Cardoso <iygorsilva@gmail.com>
2025-09-14 14:58:56 -0300

    feat: add Find the Smallest Missing Positive Integer

---
+++ challenges/find_the_smallest_missing_positive_integer.rb
@@ -0,0 +1,40 @@
+# LINK: https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/find-smallest-missing-positive-integer/problem?isFullScreen=true
+
+def first_missing_positive(nums)
+  n = nums.length
+  i = 0
+
+  # Rearranjo in-place
+  while i < n
+    correct_index = nums[i] - 1
+
+    if nums[i] > 0 && nums[i] <= n && nums[i] != nums[correct_index]
+      # Se nums[i] estiver no intervalor, for menor que o tmanho do array, e for diferente do elemnto na posição correta
+      # nums[i] != nums[correct_index]: Se nums[i] já é igual ao valor no lugar correto, trocar seria inútil (trocar valores iguais) e não convergiria.
+
+      # Troco os elemntos de posição (swap)
+      temp = nums[i]
+      nums[i] = nums[correct_index]
+      nums[correct_index] = temp
+
+    else
+      # Se nums[i] estiver fora do internavor (for negativo) incremento o index i
+      i += 1
+    end
+  end
+
+  puts nums
+
+  # Busca do menor positivo ausente
+  (0...n).each do |i|
+    return i + 1 if nums[i] != i + 1
+  end
+
+  # Caso todos estejam corretos
+  n + 1
+end
+
+
+test = first_missing_positive([10, 2, -1, 1])
+puts "\n\n"
+puts test
