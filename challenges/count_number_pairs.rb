8b5ca0150c0036483b9019846ae939821abb48b1
Igor Cardoso <iygorsilva@gmail.com>
2025-09-14 15:52:39 -0300

    feat: add Count Number Pairs

---
+++ challenges/count_number_pairs.rb
@@ -0,0 +1,15 @@
+# LINK: https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/count-number-pairs/problem?isFullScreen=true
+
+
+
+def countAffordablePairs(prices, budget)
+  count = 0
+  (0..prices.length).each do |i|
+    (i+1..prices.length-1).each do |j|
+      count += 1 if i < j && prices[i] + prices[j] <= budget
+    end
+  end
+  count
+end
+
+puts countAffordablePairs([1, 2, 3, 4, 5], 7)
